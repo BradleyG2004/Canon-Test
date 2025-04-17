e26535bb0dd976bafb3d2dd1ff955cef12ff8102
BradleyG2004 <129465283+BradleyG2004@users.noreply.github.com>
2025-04-17 22:38:37 +0200

    Product list - ok

--- CanonTestCore/src/App.vue
+++ CanonTestCore/src/App.vue
@@ -1,86 +1,99 @@
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="big-div p-4 text-center">
+    <h2>Product list - 
+      <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" version="1.0" viewBox="0 0 800 167.326"
+        class="nav__logo-svg nav__logo-svg--red" width="100" height="25">
+        <g id="canon-logo">
+          <path
+            d="m130.62 151.03c-37.195 0-67.339-30.16-67.339-67.362 0-37.204 30.144-67.335 67.339-67.335 13.11 0 25.35 3.739 35.69 10.22l-35.69 57.115 67.19-38.796c-7.3-11.631-17.77-21.653-30.43-29.258-16.11-9.645-38.52-15.614-63.3-15.614-35.348 0-66.359 12.845-83.653 32.081-12.804 14.22-20.427 32.117-20.427 51.586 0 19.483 7.623 37.393 20.427 51.623 17.339 19.26 47.888 32.03 82.653 32.03 34.76 0 65.3-12.78 82.65-32.03 0.95-1.06 1.87-2.13 2.74-3.24l-2.62-9.82c-12.19 17.4-32.38 28.8-55.23 28.8">
+          </path>
+          <path
+            d="m353.38 163.27l-28.2-105.2c-4.53-17-20.01-29.495-38.44-29.495-4.78 0-9.36 0.854-13.61 2.4l-60.71 22.076h62.44l10.67 39.848c-10.35-8.908-23.83-14.328-38.59-14.328-29.31 0-53.04 19.641-53.04 43.869 0 24.23 23.73 43.9 53.04 43.9 21.1 0 39.76-10.33 51.27-26.2l6.19 23.13h48.98m-86.02-16.34c-13.52 0-24.48-10.95-24.48-24.49 0-13.52 10.96-24.48 24.48-24.48s24.49 10.96 24.49 24.48c0 13.54-10.97 24.49-24.49 24.49z">
+          </path>
+          <path
+            d="m468.36 28.593c-3.7 0-7.2 0.818-10.32 2.283l-38.98 18.166c-1.93-11.604-12.01-20.449-24.17-20.449-3.68 0-7.19 0.818-10.35 2.283l-47.59 22.175h33.46v110.22h48.98v-97.958c0-6.759 5.47-12.261 12.25-12.261 6.74 0 12.25 5.502 12.25 12.261v97.958h48.96v-110.22c0-13.517-10.97-24.457-24.49-24.457">
+          </path>
+          <path
+            d="m775.52 28.593c-3.71 0-7.23 0.818-10.4 2.283l-38.94 18.166c-1.92-11.604-12-20.449-24.16-20.449-3.68 0-7.19 0.818-10.36 2.283l-47.57 22.175h33.45v110.22h48.97v-97.958c0-6.759 5.48-12.261 12.26-12.261 6.76 0 12.23 5.502 12.23 12.261v97.958h49v-110.22c0-13.517-10.98-24.457-24.48-24.457">
+          </path>
+          <path
+            d="m652.01 97.959c0 38.311-31.05 69.361-69.35 69.361-38.33 0-69.4-31.05-69.4-69.361 0-38.301 31.07-69.366 69.4-69.366 38.3 0 69.35 31.066 69.35 69.366m-66.48-48.314c-2.18-8.162-10.57-13.007-18.73-10.813-8.15 2.185-13.02 10.58-10.83 18.741l23.79 88.697c2.2 8.17 10.57 13.01 18.73 10.85 8.16-2.21 13.01-10.6 10.82-18.76l-23.78-88.715z">
+          </path>
+        </g>
+      </svg>
+    </h2>
+    <hr>
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">📷</th>
+          <th scope="col" class="header">Code</th>
+          <th scope="col" class="header">Status</th>
+          <th scope="col" class="header">Name</th>
+          <th scope="col" class="header">Price</th>
+          <th scope="col" class="header">Weight</th>
+          <th scope="col" class="header">Release Date</th>
+          <th scope="col" class="header">Availability</th>
+          <th scope="col" class="header">Created Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in products" :key="product.id">
+          <td>{{ index + 1 }}</td>
+          <td>{{ product.product_code }}</td>
+          <td>{{ product.product_status }}</td>
+          <td>{{ product.product_name }}</td>
+          <td>{{ product.price }}💲</td>
+          <td>{{ product.weight }}</td>
+          <td>{{ product.release_date }}</td>
+          <td>{{ product.is_available ? "✅" : "❌" }}</td>
+          <td>{{ product.meta_created_date.split("T")[0] + " - " + product.meta_created_date.split("T")[1].split(".")[0] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button id="add">
+      Add a product
+    </button>
+  </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const products = ref([]);
+
+onMounted(() => {
+  axios
+    .get("/api/product/")
+    .then((response) => {
+      console.log("Réponse reçue :", response.data);
+      products.value = response.data;
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la récupération des données :", error);
+    });
+});
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.big-div {
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 1200px;
+  font-family: "Montserrat", sans-serif;
+  font-size: 15px
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header {
+  color: crimson;
+  font-weight: bold;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#add {
+  background-color: crimson;
+  color: white;
+  border-color: crimson;
 }
 </style>
--- CanonTestCore/vite.config.ts
+++ CanonTestCore/vite.config.ts
@@ -1,15 +1,16 @@
-import { fileURLToPath, URL } from "node:url";
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://canontestexercise.azurewebsites.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+      },
     },
   },
-});
+})
